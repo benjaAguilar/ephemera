2fe3fd26db00ac6aa3fde2b99a78837f32a6cc39
Benja Aguilar <benjaaguilar02@gmail.com>
2026-07-26 09:35:14 -0300

    feat: Serve build client on prod
    add public path and use express.static to serve client build on
    production.
    use route '/{*splat}' to redirect all web traffic to index.html.

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import router from './routes/index.js';
 
 const app = express();
@@ -7,5 +9,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+if (process.env.NODE_ENV === 'production') {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const publicPath = path.join(__dirname, '../public');
+  app.use(express.static(publicPath));
+
+  app.get('/{*splat}', (_req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+  });
+}
 
 export default app;
