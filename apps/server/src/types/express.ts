7b51ace84d287b7b4089a9fd78ba32f36b39be0a
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-05 07:26:48 -0300

    types(express): add express params types

---
+++ apps/server/src/types/express.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+export type Request = express.Request;
+export type Response = express.Response;
+export type Next = express.NextFunction;
+export type Error = express.ErrorRequestHandler;
