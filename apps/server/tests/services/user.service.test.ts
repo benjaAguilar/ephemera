679d522a196e36962795cc73076070b9917d5968
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-11 09:56:14 -0300

    test(user.service): add updateExpiration() test coverage

--- apps/server/tests/services/user.service.test.ts
+++ apps/server/tests/services/user.service.test.ts
@@ -24,4 +24,32 @@ describe('UserService', () => {
       expect(prismaUserMock.create).toHaveBeenCalledWith('rick');
     });
   });
+
+  describe('updateExpiration()', () => {
+    it('should update user expiration', () => {
+      const expiration = Math.floor(Date.now() / 1000) + 3600;
+
+      verifyJwtMock.mockReturnValue({
+        userId: 3,
+        exp: expiration,
+        iat: expiration,
+      });
+
+      userService.updateExpiration(3, 'super-token');
+
+      expect(prismaUserMock.updateExpiration).toHaveBeenCalledOnce();
+      expect(prismaUserMock.updateExpiration).toHaveBeenCalledWith(3, new Date(expiration * 1000));
+    });
+
+    it('should not create an user when token verification fails', () => {
+      verifyJwtMock.mockImplementation(() => {
+        throw new UnauthorizedError('Invalid or expired token');
+      });
+
+      expect(() => {
+        userService.updateExpiration(3, 'super-invalid-token');
+      }).toThrow(UnauthorizedError);
+      expect(prismaUserMock.updateExpiration).not.toHaveBeenCalled();
+    });
+  });
 });
