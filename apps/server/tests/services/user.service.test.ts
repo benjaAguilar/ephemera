85bf7136903c9d5c272c225997061fe474adcd6f
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-11 09:40:29 -0300

    test(user.service): ref create() tests

--- apps/server/tests/services/user.service.test.ts
+++ apps/server/tests/services/user.service.test.ts
@@ -10,35 +10,18 @@ vi.mock('../../src/utils/jwtUtils.ts', () => ({
 
 const prismaUserMock: UserRepository = {
   create: vi.fn(),
+  updateExpiration: vi.fn(),
 };
 const verifyJwtMock = vi.mocked(verifyJWT);
 const userService = createUserService(prismaUserMock);
 
 describe('UserService', () => {
   describe('create()', () => {
-    it('should create an user whit token expiration date', () => {
-      const expiration = Math.floor(Date.now() / 1000) + 3600;
-
-      verifyJwtMock.mockReturnValue({
-        userId: 1,
-        exp: expiration,
-        iat: expiration,
-      });
-      userService.create('rick', 'super-valid-token');
+    it('should create an user', () => {
+      userService.create('rick');
 
       expect(prismaUserMock.create).toHaveBeenCalledOnce();
-      expect(prismaUserMock.create).toHaveBeenCalledWith('rick', new Date(expiration * 1000));
-    });
-
-    it('should not create an user when token verification fails', () => {
-      verifyJwtMock.mockImplementation(() => {
-        throw new UnauthorizedError('Invalid or expired token');
-      });
-
-      expect(() => {
-        userService.create('rick', 'super-invalid-token');
-      }).toThrow(UnauthorizedError);
-      expect(prismaUserMock.create).not.toHaveBeenCalled();
+      expect(prismaUserMock.create).toHaveBeenCalledWith('rick');
     });
   });
 });
