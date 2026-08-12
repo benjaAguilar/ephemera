6cf037d138425814f6f09d5014c36fb93b095b02
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-12 10:40:52 -0300

    feat(routes): add auth routes and router

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import authRouter from './auth.routes.js';
 const router = Router();
 
 router.get('/', (_req, res) => res.json({ message: 'Hello World' }));
+router.get('/auth', authRouter);
 
 export default router;
