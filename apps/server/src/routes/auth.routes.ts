91bbcfa54aba55dd09e4ee3ea2ad23e37005b797
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-16 18:46:16 -0300

    feat(auth.routes): add '/kill' route with route protection

--- apps/server/src/routes/auth.routes.ts
+++ apps/server/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { authController } from '../controllers/index.js';
 import { tryCatch } from '../utils/errorCatch.js';
+import { jwtAuthStrategy } from '../middlewares/passport.js';
 
 const authRouter = Router();
 
 authRouter.post('/', tryCatch(authController.auth));
+authRouter.post('/kill', jwtAuthStrategy, tryCatch(authController.killSession));
 
 export default authRouter;
