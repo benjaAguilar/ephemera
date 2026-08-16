2c25420ef159a5d778f4073a03122670c1e951cf
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-16 18:45:33 -0300

    fix(routes/index): replace get for use at '/auth'

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -3,6 +3,6 @@ import authRouter from './auth.routes.js';
 const router = Router();
 
 router.get('/', (_req, res) => res.json({ message: 'Hello World' }));
-router.get('/auth', authRouter);
+router.use('/auth', authRouter);
 
 export default router;
