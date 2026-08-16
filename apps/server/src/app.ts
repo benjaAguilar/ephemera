f7f5ee4e7d4c77c597b3ed1a48d4c72b93d7baaf
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-16 18:51:15 -0300

    fix(app.ts): move routes after cookie and passport configuration

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -13,11 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', router);
-
 app.use(cookieParser());
+
 configurePassport(passport, userService);
 app.use(passport.initialize());
+
+app.use('/api', router);
 
 if (process.env.NODE_ENV === 'production') {
   const __filename = fileURLToPath(import.meta.url);
