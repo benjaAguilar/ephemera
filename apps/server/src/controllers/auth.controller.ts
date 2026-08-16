20c727088b5a5e6ca10bf1f220ba3fe7db7dc75d
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-16 18:45:04 -0300

    feat(auth.controller): add killSession controller
    this controller gets auth user and deletes it using user service.
    then sends 200 clearing the cookie on the client.

--- apps/server/src/controllers/auth.controller.ts
+++ apps/server/src/controllers/auth.controller.ts
@@ -2,15 +2,17 @@ import { RegisterSchema } from '@ephemera/schemas';
 import type { UserService } from '../services/user.service.js';
 import type { Request, Response } from '../types/express.js';
 import { createJWT } from '../utils/jwtUtils.js';
-import { calcExpiration, validateData } from '../utils/utils.js';
+import { calcExpiration, getAuthenticatedUser, validateData } from '../utils/utils.js';
 
 interface AuthController {
   auth(req: Request, res: Response): Promise<void>;
+  killSession(req: Request, res: Response): Promise<void>;
 }
 
 export function createAuthController(userService: UserService): AuthController {
   return {
     async auth(req, res) {
+      //TODO: fix bug, if username/ttl = undefined app crashes (see issue #130)
       const { username, ttl } = req.body;
       const data = validateData(RegisterSchema, { username, ttl });
 
@@ -30,6 +32,23 @@ export function createAuthController(userService: UserService): AuthController {
         })
         .json({
           message: 'User Created and authenticated',
+        });
+    },
+
+    async killSession(req, res) {
+      const user = getAuthenticatedUser(req);
+
+      const delUser = await userService.delete(user.id);
+
+      res
+        .status(200)
+        .clearCookie('authToken', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: 'none',
+        })
+        .json({
+          message: `User ${delUser.username} has expired`,
         });
     },
   };
