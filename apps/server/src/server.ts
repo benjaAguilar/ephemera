f6c79b02a7d5bdae32958ccda581029501c5fb1b
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-18 09:44:39 -0300

    feat(server.ts): add cleanupScheduler at server

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,4 +1,8 @@
 import app from './app.js';
+import { startCleanupScheduler } from './schedulers/cleanup.scheduler.js';
+import { cleanupService } from './services/index.js';
+
+startCleanupScheduler(cleanupService);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
