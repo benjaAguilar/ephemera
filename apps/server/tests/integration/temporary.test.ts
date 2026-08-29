af0c6ce24a768cca1ee4ce0332acf53ad1b0183b
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-29 11:39:47 -0300

    test(temporary): add temporary integration test

---
+++ apps/server/tests/integration/temporary.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+import request from 'supertest';
+import app from '../../src/app.js';
+
+describe('get /api', () => {
+  it('should return 200 and hello world', async () => {
+    const res = await request(app).get('/api');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({ message: 'Hello World' });
+  });
+});
