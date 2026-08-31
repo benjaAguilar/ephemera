6d5f6fa9916602e39f88f6cac4d32b05b7a20edf
Benja Aguilar <benjaaguilar02@gmail.com>
2026-08-31 20:26:52 -0300

    test(auth.test): add auth integration tests

---
+++ apps/server/tests/integration/auth/auth.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app, prisma } from '../setup.js';
+
+describe('Auth Integration', () => {
+  describe('POST /api/auth', () => {
+    it('should response 200 and create the user at the DB', async () => {
+      const res = await request(app).post('/api/auth').send({
+        username: 'pickle',
+        ttl: '30m',
+      });
+
+      const user = await prisma.user.findUnique({
+        where: {
+          username: 'pickle',
+        },
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.headers['set-cookie']).toBeDefined();
+
+      expect(user).not.toBeNull();
+      expect(user).toMatchObject({
+        username: 'pickle',
+      });
+    });
+
+    it('should throw a 409 if user already exists', async () => {
+      await prisma.user.create({
+        data: {
+          username: 'lilo',
+        },
+      });
+
+      const res = await request(app).post('/api/auth').send({
+        username: 'lilo',
+        ttl: '15m',
+      });
+
+      expect(res.status).toBe(409);
+      expect(res.body).toMatchObject({
+        message: 'Username already taken',
+      });
+    });
+
+    it('should throw a 400 if no body is given', async () => {
+      const res = await request(app).post('/api/auth');
+
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: 'Bad request',
+      });
+    });
+  });
+});
