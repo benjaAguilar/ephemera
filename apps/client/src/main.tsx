a700f1e8a4b0c8c2240cd86f5f9591d6d782a383
Benja Aguilar <benjaaguilar02@gmail.com>
2026-09-07 10:52:52 -0300

    feat(main.tsx): add auth provider and temporary protected route

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,15 +2,23 @@ import { StrictMode } from 'react';
 import { Hero } from './pages/Hero';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { AuthProvider } from './auth/AuthProvider';
+import App from './App';
+import { ProtectRoute } from './auth/ProtectRoute';
 
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Hero />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Hero />} />
+          <Route path="app" element={<ProtectRoute />}>
+            <Route index element={<App />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 );
