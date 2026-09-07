af3544ffe8340b6206825f9c360306d89176f373
Benja Aguilar <benjaaguilar02@gmail.com>
2026-09-07 11:39:20 -0300

    ref(main): refactor app route for chats route

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,8 +3,8 @@ import { Hero } from './pages/Hero';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AuthProvider } from './auth/AuthProvider';
-import App from './App';
 import { ProtectRoute } from './auth/ProtectRoute';
+import { Chats } from './pages/Chats';
 
 const root = document.getElementById('root');
 
@@ -14,8 +14,8 @@ ReactDOM.createRoot(root!).render(
       <BrowserRouter>
         <Routes>
           <Route index element={<Hero />} />
-          <Route path="app" element={<ProtectRoute />}>
-            <Route index element={<App />} />
+          <Route path="chats" element={<ProtectRoute />}>
+            <Route index element={<Chats />} />
           </Route>
         </Routes>
       </BrowserRouter>
